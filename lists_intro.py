f3a72a534f4fb9791da7633964006d0d6435099c
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-06 19:06:44 -0500

    bascis of lists, indexing and slicing, lists are immutable

---
+++ lists_intro.py
@@ -0,0 +1,14 @@
+computer_parts = ["computer", "monitor", "keyboard", "mouse", "mouse pad"]
+
+for part in computer_parts:
+    print(part)
+
+print()
+print(computer_parts[2]) # keyboard
+
+print(computer_parts[0:3]) # computer, monitor, keyboard
+print(computer_parts[-1]) # mouse pad
+
+# STRINGS ARE IMMUTABLE THEY CAN NOT BE CHANGED
+
+
