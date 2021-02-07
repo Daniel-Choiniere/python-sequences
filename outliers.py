78509bab5a3d490c3b8668c005302b793d7a1c1a
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-07 14:33:24 -0500

    safely removing items from a list

---
+++ outliers.py
@@ -0,0 +1,30 @@
+data = [4, 5, 104, 105, 110, 120, 130, 130, 150,
+        160, 170, 183, 185, 187, 188, 191, 350, 360]
+
+# del data[0:2]
+# print(data)
+# del data[14:]
+# print(data)
+
+min_valid = 100
+max_valid = 200
+
+# this will not work because we delete the an item therefore changing the
+# index position of the items so they will skipped over
+# for index, value in enumerate(data):
+#     if(value < min_valid) or (value > max_valid):
+#         del data[index]
+#
+# print(data)
+
+# process the low values in the list
+stop = 0
+for index, value in enumerate(data):
+    if value >= min_valid:
+        stop = index
+        break
+print(stop)
+del data[:stop]
+print(data)
+
+
