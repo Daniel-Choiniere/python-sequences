cb2f4d74a601a9a2be43f2baa55ac1945c689896
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-07 16:09:09 -0500

    deleting the too high values from the list safely

--- outliers.py
+++ outliers.py
@@ -23,8 +23,20 @@ for index, value in enumerate(data):
     if value >= min_valid:
         stop = index
         break
-print(stop)
+print(stop) # for debugging
 del data[:stop]
 print(data)
 
+# process the high values in the list
+start = 0
+for index in range(len(data) - 1, -1, -1):
+    if data[index] <= max_valid:
+        # We have the index of the last item we want to keep
+        # Set start to the position of the first item to delete,
+        # which is 1 after index
+        start = index + 1
+        break
+print(start)
+del data[start:]
+print(data)
 
