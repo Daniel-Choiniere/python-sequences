a16e9299d3e7764a73c7288571f0c92f55db79f1
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-12 16:55:46 -0500

    more on tuples, unpacking

---
+++ unpacking_tuples.py
@@ -0,0 +1,24 @@
+a = b = c = d = e = f = 12
+print(c)
+
+x, y, z = 1, 2, 76
+print(x)
+print(y)
+print(z)
+
+print("Unpacking a tuple")
+data = 1, 2, 76  # data representing a tuple
+x, y, z = data
+print(x)
+print(y)
+print(z)
+
+print("Unpacking a list")
+data_list = [12, 13, 14]  # data representing a list
+p, q, r = data_list
+print(p)
+print(q)
+print(r)
+
+# another advantage of tuples is that you can always unpack a tuple
+# because a tuple cant be changed you always know how many items there are to unpack
