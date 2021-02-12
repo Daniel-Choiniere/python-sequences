0e5c9824da58ba55b99483396ecfac6d47c5ee63
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-12 16:43:24 -0500

    more on tuples, tuple are immutable

--- tuple_intro.py
+++ tuple_intro.py
@@ -10,3 +10,22 @@ age = 35
 print(name, age, "Python", 2021)
 # A tuple
 print((name, age, "Python", 2021))
+
+welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+bad = "Bad Company", "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda May", 2011
+metallica = "Ride the Lightning", "Metallica", 1984
+
+# still use square brackets when accessing tuples
+# Tuples are IMMUTABLE, this protects the integrity of your data
+# Tuples use less memory than lists
+print(metallica)
+print(metallica[0])
+print(metallica[1])
+print(metallica[2])
+
+# Converting a tuple to a list
+metallica2 = list(metallica)
+print(metallica2)
+
