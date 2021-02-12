7e4afd112c362ad1066274b4e12cfdaf9da3cd24
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-12 16:31:47 -0500

    intro to tuples

---
+++ tuple_intro.py
@@ -0,0 +1,12 @@
+# use parentheses instead of brackets to create a tuple
+t = ("a", "b", "c")
+print(t)
+
+
+name = "Dan"
+age = 35
+
+# Not a tuple
+print(name, age, "Python", 2021)
+# A tuple
+print((name, age, "Python", 2021))
