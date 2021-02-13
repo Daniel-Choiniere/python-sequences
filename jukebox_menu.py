5e9321e7160e474b64e0d832091e28a5c23b75ca
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-13 00:25:59 -0500

    we have out jukebox app allowing the user to choose an album

--- jukebox_menu.py
+++ jukebox_menu.py
@@ -1,9 +1,19 @@
 # imported the albums data list from the nested_data file
 from nested_data import albums
 
+# Constants are written in all UPPERCASE
+SONGS_LIST_INDEX = 3
 while True:
     print("Please choose your album (an invalid choice exits)")
     for index, (title, artist, year, songs) in enumerate(albums):
-        print("{}, {}, {}, {}, {}"
-              .format(index + 1, title, artist, year, songs))
-    break
+        print("{}: {}".format(index + 1, title))
+
+    choice = int(input())
+    if 1 <= choice <= len(albums):
+        songs_list = albums[choice - 1][SONGS_LIST_INDEX]
+    else:
+        break
+
+    print(albums[choice - 1])
+    print(songs_list)
+    print()
