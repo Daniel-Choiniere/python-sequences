33e71df3ac6da099a029ea01b51dd567834b9c82
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-13 00:34:48 -0500

    jukebox app is completed and working as intended, user can pick an album and a song off that album to play

--- jukebox_menu.py
+++ jukebox_menu.py
@@ -3,6 +3,8 @@ from nested_data import albums
 
 # Constants are written in all UPPERCASE
 SONGS_LIST_INDEX = 3
+SONG_TITLE_INDEX = 1
+
 while True:
     print("Please choose your album (an invalid choice exits)")
     for index, (title, artist, year, songs) in enumerate(albums):
@@ -14,6 +16,15 @@ while True:
     else:
         break
 
-    print(albums[choice - 1])
-    print(songs_list)
-    print()
+    print("Please choose your song:")
+    for index, (track_number, song) in enumerate(songs_list):
+        print("{}: {}".format(index + 1, song))
+
+    song_choice = int(input())
+    if 1 <= song_choice <= len(songs_list):
+        title = songs_list[song_choice - 1][SONG_TITLE_INDEX]
+    else:
+        break
+
+    print("Playing {}".format(title))
+    print("=" * 40)
